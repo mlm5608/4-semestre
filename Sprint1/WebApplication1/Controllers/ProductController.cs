9192b4808c8a3bbc07fc1417b915389a57b3e842
Miguel Lamarca Moreira <50866670890@info.local>
2024-07-26 17:22:09 -0300

    att

---
+++ Sprint1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using WebApplication1.Domains;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMongoCollection<Product> _product;
+
+        public ProductController(MongoDbService service)
+        {
+            _product = service.GetDatabase.GetCollection<Product>("product");
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            try
+            {
+                var products = await _product.Find(FilterDefinition<Product>.Empty).ToListAsync();
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Post(Product product)
+        {
+            try
+            {
+                await _product.InsertOneAsync(product);
+                return Ok(product);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("GetByID")]
+        public async Task<ActionResult> GetById(string id)
+        {
+            try
+            {
+                var lista = await _product.Find(Builders<Product>.Filter.Eq(m => m.Id, id).ToListAsync()
+                return Ok(lista);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(string id)
+        {
+            try
+            {
+                await _product.DeleteOneAsync(Builders<Product>.Filter.Eq(m => m.Id, id));
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Product p, string id)
+        {
+            try
+            {
+                p.Id = id;
+                await _product.FindOneAndReplaceAsync(Builders<Product>.Filter.Eq(m => m.Id, id), p);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Sprint1/WebApplication1/Services/MongoDbService.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver;
+
+namespace WebApplication1.Services
+{
+    public class MongoDbService
+    {
+        /// <summary>
+        /// Armazenar a configuração da aplicação
+        /// </summary>
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Armazena uma ref ao MongoDb
+        /// </summary>
+        private readonly IMongoDatabase _database;
+
+        /// <summary>
+        /// Contém a configuração para acesso ao DB
+        /// </summary>
+        /// <param name="configuration">Objeto contendo toda a config da aplicação</param>
+        public MongoDbService(IConfiguration configuration)
+        {
+            //atribui a config recebida em _config
+            _config = configuration;
+
+            //Acessa a string de conexão
+            var connectionString = _config.GetConnectionString("DbConnection");
+            
+            //Transforma a string obtida em MongoUrl
+            var mongoUrl = MongoUrl.Create(connectionString);
+
+            //Cria um client 
+            var mongoClient = new MongoClient(mongoUrl);
+
+            //Obtém a ref ao MongoDb
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+        /// <summary>
+        /// Prop para acessar o db => retorna os dados em _Database
+        /// </summary>
+        public IMongoDatabase GetDatabase => _database;
+    }
+}
