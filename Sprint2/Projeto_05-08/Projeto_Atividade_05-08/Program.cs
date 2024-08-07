1a0b8d9aa95ee001d41256cb4050d022c62d3830
Miguel Lamarca Moreira <50866670890@info.local>
2024-08-07 14:30:50 -0300

    fim api com testes

--- Sprint2/Projeto_05-08/Projeto_Atividade_05-08/Program.cs
+++
@@ -1,9 +0,0 @@
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
-        app.Run();
-    }
-}
