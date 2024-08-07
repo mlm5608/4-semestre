5e8273feb164f6fffbac2730a4c0d4ca928bce0e
Miguel Lamarca Moreira <50866670890@info.local>
2024-08-07 14:26:46 -0300

    fim testes com api

--- Sprint2/Projeto_05-08/Projeto_Atividade_05-08/TesteApiUnit.Test/UnitTest1.cs
+++ Sprint2/Projeto_05-08/Projeto_Atividade_05-08/TesteApiUnit.Test/UnitTest1.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Moq;
 using Projeto_Atividade_05_08.Domains;
 using ProjetoAtividade0508.Interfaces;
+using ProjetoAtividade0508.Repositories;
 
 namespace TesteApiUnit.Test
 {
@@ -78,19 +79,70 @@ namespace TesteApiUnit.Test
         [Fact]
         public void GetById()
         {
-            var produtoparabuscar = new Products { IdProduct = Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"), Name = "produto1", Price = 10 };
-            var products = new List<Products>
+
+            List<Products> productList = new List<Products>();
+
+
+            Products product = new Products
             {
-                produtoparabuscar,
-                new Products{ IdProduct = Guid.NewGuid() , Name = "produto2", Price= 20 }
+                IdProduct = Guid.NewGuid(),
+                Name = "Óculos da Catarina",
+                Price = 32
             };
+
+            productList.Add(product);
+
+
+            var productBuscado = productList.FirstOrDefault(x => x.IdProduct == product.IdProduct);
+
+
             var mockRepository = new Mock<IProductRepository>();
 
-            mockRepository.Setup(x => x.BuscarPorId(Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"))).Returns(produtoparabuscar);
+            mockRepository.Setup(x => x.BuscarPorId(product.IdProduct)).Returns(productBuscado);
 
-            mockRepository.Object.BuscarPorId(Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"));
+            mockRepository.Object.BuscarPorId(product.IdProduct);
 
-            Assert.True()
+            Assert.Equal(1, productList.Count);
         }
+
+        /// <summary>
+        /// Teste para a funcionalidade de atualizar um produto
+        /// </summary>
+        [Fact]
+        public void Put()
+        {
+
+            List<Products> productList = new List<Products>();
+
+
+            Products product = new Products
+            {
+                IdProduct = Guid.NewGuid(),
+                Name = "Óculos da Catarina",
+                Price = 32
+            };
+            productList.Add(product);
+
+
+            var mockRepository = new Mock<IProductRepository>();
+
+
+            mockRepository.Setup(x => x.Atualizar(product, product.IdProduct)).Callback<Guid, Products>((id, p) =>
+            {
+                var item = productList.FirstOrDefault(x => x.IdProduct == id);
+
+                if (item != null)
+                {
+                    p.Name = "Óculos do Matheus Lima";
+                    p.Price = 35;
+                    productList.Add(product);
+                }
+            });
+
+            mockRepository.Object.Atualizar(product, product.IdProduct);
+
+            Assert.Equal("Óculos do Matheus Lima", product.Name);
+        }
+
     }
 }
